7bb4bcff12fac07b09f679944d34761809468ab9
BP <lordterrin@gmail.com>
2022-04-20 21:59:22 -0700

    test2

--- api/create_user.php
+++ api/create_user.php
@@ -1,6 +1,6 @@
 <?php
 
-sleep(2);
+sleep(1);
 
 require $_SERVER['DOCUMENT_ROOT'] .'/legion/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . '/legion');
@@ -56,7 +56,8 @@ $created_at = date("Y-m-d H:i:s", time());
                 mysqli_stmt_store_result($stmt);
 
                 if(mysqli_stmt_num_rows($stmt) == 1){
-                    $username_err = "This username is already taken.";
+                    echo "This username is already taken.";
+                    die();
                 } else{
                     $username = trim($_POST["user_username"]);
                 }
@@ -73,44 +74,41 @@ $created_at = date("Y-m-d H:i:s", time());
 
     // Validate password
     if(empty(trim($_POST["user_password"]))){
-        $password_err = "Please enter a password.";
+        echo "Please enter a password.";
+        die();
     } elseif(strlen(trim($_POST["user_password"])) < 3){
-        $password_err = "Password must have atleast 3 characters.";
+        echo "Password must have atleast 3 characters.";
+        die();
     } else{
         $password = trim($_POST["user_password"]);
     }
 
 
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) ){
 
-        // Prepare an insert statement
-        $sql = "INSERT INTO legion_data.users (username, password) VALUES (?, ?)";
 
-        if($stmt = mysqli_prepare($conn, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+    // Prepare an insert statement
+    $sql = "INSERT INTO legion_data.users (username, password) VALUES (?, ?)";
 
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+    if($stmt = mysqli_prepare($conn, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
 
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                echo "Success!";
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
+        // Set parameters
+        $param_username = $username;
+        $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
-            // Close statement
-            mysqli_stmt_close($stmt);
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            echo "Success!";
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
         }
+
+        // Close statement
+        mysqli_stmt_close($stmt);
     } else {
-    	if ( $username_err ) {
-    		echo $username_err;
-    	} else if ( $password_err ) {
-    		echo $password_err;
-    	}
+    	echo "error";
     }
 
     // Close connection
