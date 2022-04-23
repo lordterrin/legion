dc35c8919801ad94002ef99f417ed10857fa0399
BP <lordterrin@gmail.com>
2022-04-22 20:41:50 -0700

    added unit text audit table

--- api/change_unit_text.php
+++ api/change_unit_text.php
@@ -16,6 +16,7 @@ $dotenv = Dotenv\Dotenv::createImmutable($_SERVER['DOCUMENT_ROOT'] . '/legion');
 $dotenv->load();
 
 $environment = $_ENV['environment'];
+$created_at  = date("Y-m-d H:i:s", time());
 
 if ( $environment !== 'prod') {
 	$servername = $_ENV['local_servername'];
@@ -39,15 +40,33 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+$sql = "SELECT basic_strategy from legion_data.units WHERE id = $unit_id;";
+$result = $conn->query($sql);
+
+while($row = $result->fetch_assoc()) {
+	$output[] = $row;
+}
+
+try {
+	$previous_basic_strategy = $output[0]['basic_strategy'] ?? '';
+} catch(Exception $e) {
+	$previous_basic_strategy = '';
+}
+
+/* update the units table */
 $stmt = $conn->prepare("UPDATE legion_data.units SET basic_strategy = ? , updated_by = ? where id = $unit_id");
 $stmt->bind_param("si", $new_unit_text, $user_id);
 $stmt->execute();
 
-//$result = $conn->query($sql);
 
-$output = [];
-// while($row = $result->fetch_assoc()) {
-// 	$output[] = $row;
-// }
+/* create audit record */
+$sql = "
+	INSERT INTO legion_data.description_audit
+	(user_id, unit_id, previous_description, new_description, updated_at)
+	VALUES
+	($user_id, $unit_id, '$previous_basic_strategy', '$new_unit_text', '$created_at');";
+
+$result = $conn->query($sql);
+
 
 echo "update successful";
